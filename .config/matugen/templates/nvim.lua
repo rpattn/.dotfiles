83a2ac39c37147ecb399980461de4b3af61391e9
Robert Patton <robpatton04@gmail.com>
2026-05-04 14:22:14 +0100

    add matugen

---
+++ .config/matugen/templates/nvim.lua
@@ -0,0 +1,14 @@
+return {
+  primary = "{{colors.primary.default.hex}}",
+  secondary = "{{colors.secondary.default.hex}}",
+  tertiary = "{{colors.tertiary.default.hex}}",
+
+  surface = "{{colors.surface.default.hex}}",
+  surface_container = "{{colors.surface_container.default.hex}}",
+  surface_variant = "{{colors.surface_variant.default.hex}}",
+
+  on_surface = "{{colors.on_surface.default.hex}}",
+  outline = "{{colors.outline.default.hex}}",
+
+  error = "{{colors.error.default.hex}}",
+}
